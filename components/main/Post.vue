a811ab8292f9630fa6db43cbd8053e6d93475a03
woodtick88 <woodtick88@gmail.com>
2019-06-13 15:35:34 +0300

    main page

---
+++ components/main/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="post">
+
+  </article>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style lang="sass" scoped>
+
+.post
+  height: 200px
+  border: 1px solid black
+  margin-bottom: 2rem
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    Hello Nuxt
-  </div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <app-post
+        v-for="post in 3"
+        :key="post"
+      ></app-post>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from '@/components/main/Post'
 
 export default {
+  head: {
+    title: 'Главная'
+  },
+  components: {
+    AppPost
+  }
 }
 </script>
 
