af5ab1c71fbc588054f9886a872481275accc8b7
woodtick88 <woodtick88@gmail.com>
2019-06-13 19:57:34 +0300

    Post component

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,13 +1,44 @@
 <template>
-  <article class="post">
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
 
-  </article>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+        alt="post image"
+        class="post-img"
+      >
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+
+      <span>
+        <i class="el-icon-message"></i>
+
+        12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 
 export default {
-
+  methods: {
+    openPost() {
+      console.log('Open Post')
+    }
+  }
 }
 
 </script>
@@ -15,8 +46,25 @@ export default {
 <style lang="sass" scoped>
 
 .post
-  height: 200px
-  border: 1px solid black
-  margin-bottom: 2rem
+  margin-bottom: 1.5rem
+
+  .el-card__body
+    padding-left: 0rem !important
+
+.post-header
+  display: flex
+  justify-content: space-between
+  align-items: center
+
+.post-img
+  width: 100%
+  height: auto
+
+.post-footer
+  display: flex
+  justify-content: space-between
+  align-items: center
+  padding: 1rem
+
 
 </style>
