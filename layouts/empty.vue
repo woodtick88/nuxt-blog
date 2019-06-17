b3d7f10c4eddeee613a0c1be064f7724e1a87366
woodtick88 <woodtick88@gmail.com>
2019-06-17 20:30:22 +0300

    login page

---
+++ layouts/empty.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="empty-layout">
+        <nuxt />
+    </div>            
+</template>
+
+<style lang="sass" scoped>
+
+.empty-layout
+    display: flex
+    justify-content: center
+    width: 100%
+    padding-top: 4rem
+
+</style>
---
+++ pages/admin/login.vue
@@ -0,0 +1,70 @@
+<template>
+    <el-card
+        shadow="always"
+        :style="{width: '500px'}"
+    >
+        <el-form 
+            :model="controls" 
+            :rules="rules" 
+            ref="form"
+            @submit.native.prevent="onSubmit"
+        >
+            <h1>Добавить комментарий</h1>
+            <el-form-item label="Логин" prop="login">
+                <el-input v-model.trim="controls.login"></el-input>
+            </el-form-item>
+            <div class="mb2">
+                <el-form-item label="Пароль" prop="password">
+                    <el-input 
+                        v-model.trim="controls.password" 
+                        type="password"                   
+                    >
+                    </el-input>
+                </el-form-item>
+            </div>
+            <el-form-item>
+                <el-button
+                    type="primary"
+                    round
+                    native-type="submit"
+                    :loading="loading"
+                >
+                    Добавить комментарий
+                </el-button>
+            </el-form-item>
+        </el-form> 
+    </el-card>
+</template>
+
+<script>
+export default {
+    layout: 'empty',
+    data() {
+        return {
+            loading: false,
+            controls: {
+                login: '',
+                password: ''
+            },
+            rules: {
+                login: [
+                    { required: true, message: 'Введите логин', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: 'Введите пароль', trigger: 'blur' },
+                    { min: 6, message: 'Пароль должен быть не менее 6 символов', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log('hhh')
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
