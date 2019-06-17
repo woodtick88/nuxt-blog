9f8a12d0e1e7e24bc2974d9b9a2fc26bda56122d
woodtick88 <woodtick88@gmail.com>
2019-06-17 13:39:34 +0300

    admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="admin-layout-wrap">
+        <el-container :style="{height: '100%'}">
+            <el-aside width="200px">
+                Aside
+            </el-aside>
+            <nuxt />    
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="sass" scoped>
+
+.admin-layout-wrap
+    width: 100%
+    height: 100vh
+
+</style>
+
---
+++ pages/admin/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Wlcome to admin page</h1>
+</template>
+
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
+
