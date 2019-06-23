ae62f7ee1b1b30e7d1704013f75c9d0581caff12
woodtick88 <woodtick88@gmail.com>
2019-06-23 11:53:32 +0300

    add body-parser and keys file

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const keys = require('./keys')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+module.exports = app
+
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const app = require('./app')
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
