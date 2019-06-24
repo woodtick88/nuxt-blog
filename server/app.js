675aa42e890fa7ab7c6b50164d3f5c347ff05b5e
woodtick88 <woodtick88@gmail.com>
2019-06-24 16:32:16 +0300

    backend: tuning passport
    backend: tuning passport
    add plugins: passport, passport-jwt

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
@@ -8,6 +10,9 @@ const app = express()
 mongoose.connect(keys.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(error => console.error(error))
+
+app.use(passport.initialize()) 
+passport.use(passportStrategy)   
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
