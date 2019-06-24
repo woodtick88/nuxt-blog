2d84a30ba478497877e33395ca139da4529b9a39
woodtick88 <woodtick88@gmail.com>
2019-06-24 20:33:59 +0300

    backend: posts routes

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -3,3 +3,24 @@ const Post = require('../models/post.model')
 module.exports.create = async (req, res) => {
 
 }
+
+module.exports.getAll = async (req, res) => {
+
+}
+
+module.exports.getById = async (req, res) => {
+
+}
+
+module.exports.update = async (req, res) => {
+
+}
+
+module.exports.remove = async (req, res) => {
+
+}
+
+
+module.exports.addView = async (req, res) => {
+
+}
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,5 +1,51 @@
 const passport = require('passport')
 const {Router} =  require('express')
+const ctr = require('../controllers/post.controller')
 const router = new Router()
+
+
+// Admin
+// /api/post/admin
+
+router.post(
+    '/admin/',
+    passport.authenticate('jwt', {session: false}),
+    ctr.create
+)
+
+router.get(
+    '/admin/',
+    passport.authenticate('jwt', {session: false}),
+    ctr.getAll
+)
+
+router.get(
+    '/admin/:id',
+    passport.authenticate('jwt', {session: false}),
+    ctr.getById
+)
+
+router.put(
+    '/admin/:id',
+    passport.authenticate('jwt', {session: false}),
+    ctr.update
+)
+
+router.delete(
+    '/admin/:id',
+    passport.authenticate('jwt', {session: false}),
+    ctr.remove
+)
+
+
+// Base 
+// /api/post
+
+router.get('/', ctr.getAll)
+router.get('/:id', ctr.getById)
+router.put('/:id', ctr.addView)
+
+
+
 
 module.exports = router
