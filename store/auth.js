84db052761fa2be6a09b92c5457ce9ce5758355c
woodtick88 <woodtick88@gmail.com>
2019-06-25 21:30:24 +0300

    login and error handling

--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-    token: true
+    token: null
 })
 
 export const mutations = {
@@ -14,8 +14,7 @@ export const mutations = {
 export const actions = {
     async login ({commit, dispatch}, formData) {
         try {
-            const {token} = this.$axios.$post('/api/auth/admin/login', formData)
-            console.log('token', token)
+            const {token} = await this.$axios.$post('/api/auth/admin/login', formData)          
             dispatch('setToken', token)
         } catch (e) {
             commit('setError', e, {root: true})
