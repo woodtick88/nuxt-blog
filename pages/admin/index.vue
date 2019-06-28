86d5a9ac16de669bf78e7009555ca2c65f268020
woodtick88 <woodtick88@gmail.com>
2019-06-28 11:42:18 +0300

    creating a schedule component

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,56 +1,28 @@
 <template>
   <div>
     <h1>Аналитика по постам</h1>
-    <canvas height="50vh" ref="canvas"></canvas>
+    <app-analytics-chart
+        title="Количество просмотров"
+        :labels="views.labels"
+        :data="views.data"
+    ></app-analytics-chart>
+    <app-analytics-chart
+        title="Количество комментариев"
+        :labels="comments.labels"
+        :data="comments.data"
+    ></app-analytics-chart>
   </div>
 </template>
 
 <script>
-import {Bar} from 'vue-chartjs'
+import AppAnalyticsChart from '@/components/admin/AnalyticsChart'
 export default {
   layout: 'admin',
+  components: {AppAnalyticsChart},
   middleware: ['admin-auth'],
-  extends: Bar,
   async asyncData({store}) {
-      const analytics = await store.dispatch('post/getAnalytics')
-      return {analytics}
-  },
-  mounted() {
-    console.log(this.analytics) 
-    const data = {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-    const options = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-    this.renderChart(data, options)
-  }
+      const {views, comments} = await store.dispatch('post/getAnalytics')
+      return {views, comments}
+  }  
 }
 </script>
